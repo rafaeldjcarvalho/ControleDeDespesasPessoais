8e5503526ac919229482f607bdc92c609697a702
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-07 10:23:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ControleDeDespesasPessoais
+Uma aplicação web full-stack que permite aos usuários gerenciar suas finanças pessoais e controlar seu dinheiro.
---
+++ src/test/java/com/rafael/ControleDeDespesasPessoais/ControleDeDespesasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.ControleDeDespesasPessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeDespesasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
