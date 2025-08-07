7a67ed70274f5905dbee81762c5f4a45f5811a7d
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-07 10:39:33 -0300

    feat: Create user class

---
+++ src/main/java/com/rafael/ControleDeDespesasPessoais/domain/entitys/User.java
@@ -0,0 +1,38 @@
+package com.rafael.ControleDeDespesasPessoais.domain.entitys;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String nome;
+	
+	@Column(nullable = false, length = 100, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	@Length(min = 8)
+	private String senha;
+	
+}
