a37a86a241edd5c155c37e3f64236e1936d321eb
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-09 15:24:48 -0300

    feat: Update Category Repository

--- src/main/java/com/rafael/ControleDeDespesasPessoais/domain/repositories/CategoryRepository.java
+++ src/main/java/com/rafael/ControleDeDespesasPessoais/domain/repositories/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.rafael.ControleDeDespesasPessoais.domain.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rafael.ControleDeDespesasPessoais.domain.entitys.Category;
@@ -11,4 +14,7 @@ import com.rafael.ControleDeDespesasPessoais.domain.entitys.Category;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 	
 	Optional<String> findByNome(String nome);
+	
+	@Query("SELECT c FROM Category c JOIN c.usuario u WHERE u.id = :usuario_id")
+	List<Category> findCategoriesByUser(@Param("usuario_id") Long usuario_id);
 }
