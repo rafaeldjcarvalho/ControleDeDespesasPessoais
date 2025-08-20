611f727ba76aedfc2dad0b2c9347a5a07350d330
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-20 14:22:58 -0300

    feat: CategoryController
    - Fixing bugs

--- src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/CategoryController.java
+++ src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/CategoryController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rafael.ControleDeDespesasPessoais.domain.dtos.CategoryDTO;
 import com.rafael.ControleDeDespesasPessoais.domain.services.CategoryService;
+import com.rafael.ControleDeDespesasPessoais.infra.security.TokenService;
 
 @Controller
 @RequestMapping("/api/categorias")
@@ -23,9 +25,17 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoriaService;
 	
-	@GetMapping("/{id_usuario}")
-	public ResponseEntity<List<CategoryDTO>> listarCategorias(@PathVariable Long id_usuario) {
-		List<CategoryDTO> lista = categoriaService.listarCategoriaDoUsuario(id_usuario);
+	@Autowired
+	private TokenService tokenService;
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoryDTO>> listarCategorias(@RequestHeader("Authorization") String token) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		List<CategoryDTO> lista = categoriaService.listarCategoriaDoUsuario(userEmail);
 		return ResponseEntity.ok(lista);
 	}
 	
@@ -36,14 +46,24 @@ public class CategoryController {
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<CategoryDTO> atualizarCategoria(@PathVariable Long id, @RequestBody CategoryDTO dados) {
-		CategoryDTO categoria = this.categoriaService.atualizarCategoria(id, dados);
+	public ResponseEntity<CategoryDTO> atualizarCategoria(@RequestHeader("Authorization") String token, @PathVariable Long id, @RequestBody CategoryDTO dados) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		CategoryDTO categoria = this.categoriaService.atualizarCategoria(id, userEmail, dados);
 		return ResponseEntity.ok(categoria);
 	}
 	
-	@DeleteMapping()
-	public ResponseEntity<?> deletarCategoria(@RequestBody CategoryDTO dados) {
-		this.categoriaService.deletarCategoria(dados.id(), dados.id_usuario());
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletarCategoria(@PathVariable Long id, @RequestHeader("Authorization") String token) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		this.categoriaService.deletarCategoria(id, userEmail);
 		return ResponseEntity.noContent().build();
 	}
 
