bc174944dbdf9565ca7c08cbd931b823fb76ef66
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-21 14:51:11 -0300

    feat: Update SummaryController
    - Fixing bugs

--- src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/FinancialSummaryController.java
+++ src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/FinancialSummaryController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rafael.ControleDeDespesasPessoais.domain.dtos.FinancialSummary;
 import com.rafael.ControleDeDespesasPessoais.domain.services.FinancialSummaryService;
+import com.rafael.ControleDeDespesasPessoais.infra.security.TokenService;
 
 @Controller
 @RequestMapping("/api/dashboard")
@@ -17,12 +19,20 @@ public class FinancialSummaryController {
 	@Autowired
 	private FinancialSummaryService summaryService;
 	
+	@Autowired
+	private TokenService tokenService;
+	
 	@GetMapping("/summary")
 	public ResponseEntity<FinancialSummary> resumoFinanceiro(
-			@RequestParam("usuario_id") Long usuario_id,
+			@RequestHeader("Authorization") String token,
 			@RequestParam("mes") Long mes,
 			@RequestParam("ano") Long ano) {
-		FinancialSummary sumario = this.summaryService.getResumoFinanceiro(usuario_id, mes, ano);
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		FinancialSummary sumario = this.summaryService.getResumoFinanceiro(userEmail, mes, ano);
 		return ResponseEntity.ok(sumario);
 	}
 }
