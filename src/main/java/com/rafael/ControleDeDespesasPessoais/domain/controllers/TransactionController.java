c6eecbf62cb0391d9ac008739257e52b412cfaa1
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-08-21 14:50:20 -0300

    feat: Update TransactionController
    - Fixing bugs

--- src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/TransactionController.java
+++ src/main/java/com/rafael/ControleDeDespesasPessoais/domain/controllers/TransactionController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rafael.ControleDeDespesasPessoais.domain.dtos.TransactionDTO;
 import com.rafael.ControleDeDespesasPessoais.domain.services.TransactionService;
+import com.rafael.ControleDeDespesasPessoais.infra.security.TokenService;
 
 @Controller
 @RequestMapping("/api/transacoes")
@@ -24,20 +26,33 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 	
-	//- /transacoes?Id_usuario=1&mes=8&ano=2025&id_categoria=3
+	@Autowired
+	private TokenService tokenService;
+	
+	//- /transacoes?mes=8&ano=2025&id_categoria=3
 	@GetMapping
 	public ResponseEntity<List<TransactionDTO>> listarTodasTransacoes(
-			@RequestParam(required = true) Long id_usuario,
+			@RequestHeader("Authorization") String token,
 			@RequestParam(required = false) Long mes,
 			@RequestParam(required = false) Long ano,
 			@RequestParam(required = false) Long id_categoria) {
-		List<TransactionDTO> lista = this.transactionService.listarTransacoes(id_usuario, mes, ano, id_categoria);
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		List<TransactionDTO> lista = this.transactionService.listarTransacoes(userEmail, mes, ano, id_categoria);
 		return ResponseEntity.ok(lista);
 	}
 	
-	@GetMapping("/{id_usuario}/{id_transacao}")
-	public ResponseEntity<TransactionDTO> listarTransacao(@PathVariable Long id_usuario, @PathVariable Long id_transacao) {
-		TransactionDTO transacao = this.transactionService.listarTransacao(id_usuario, id_transacao);
+	@GetMapping("/{id_transacao}")
+	public ResponseEntity<TransactionDTO> listarTransacao(@RequestHeader("Authorization") String token, @PathVariable Long id_transacao) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		TransactionDTO transacao = this.transactionService.listarTransacao(userEmail, id_transacao);
 		return ResponseEntity.ok(transacao);
 	}
 	
@@ -47,15 +62,28 @@ public class TransactionController {
 		return ResponseEntity.ok(novaTransacao);
 	}
 	
-	@PutMapping
-	public ResponseEntity<TransactionDTO> atualizarTransacao(@RequestBody TransactionDTO transacao) {
-		TransactionDTO novaTransacao = this.transactionService.atualizarTransacao(transacao);
+	@PutMapping("/{id_transacao}")
+	public ResponseEntity<TransactionDTO> atualizarTransacao(
+			@RequestHeader("Authorization") String token, 
+			@PathVariable Long id_transacao,
+			@RequestBody TransactionDTO transacao) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		TransactionDTO novaTransacao = this.transactionService.atualizarTransacao(id_transacao, transacao);
 		return ResponseEntity.ok(novaTransacao);
 	}
 	
-	@DeleteMapping
-	public ResponseEntity<?> deletarTransacao(@RequestBody TransactionDTO dados) {
-		this.transactionService.deletarTransacao(dados);
+	@DeleteMapping("/{id_transacao}")
+	public ResponseEntity<?> deletarTransacao(@RequestHeader("Authorization") String token, @PathVariable Long id_transacao) {
+		String[] tokenFormated = token.split(" ");
+		String userEmail = tokenService.validateToken(tokenFormated[1]);
+		if(userEmail == null) {
+			throw new RuntimeException("Token is invalid");
+		}
+		this.transactionService.deletarTransacao(id_transacao, userEmail);
 		return ResponseEntity.noContent().build();
 	}
 }
